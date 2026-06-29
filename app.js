05d6f8ce37670579b506c167a0aacc03dc4b8368
micaggalan <>
2026-06-29 16:00:04 -0300

    sume estadisticas

--- app.js
+++ app.js
@@ -71,3 +71,72 @@ function mostrarRecitales(datos){
     });
 
 }
+let recitales = [];
+
+document.addEventListener("DOMContentLoaded", async () => {
+
+    recitales = await consumirExcelDrive(ID_EXCEL);
+
+    mostrarRecitales(recitales);
+
+});
+function mostrarEstadisticas(datos){
+
+    // Cantidad de recitales
+    document.getElementById("cantRecitales").textContent = datos.length;
+
+    // Total gastado
+    const total = datos.reduce((suma, recital) => {
+
+        return suma + Number(recital["costo de la entrada"]);
+
+    },0);
+
+    document.getElementById("totalGastado").textContent =
+        "$" + total.toLocaleString("es-AR");
+
+    // Puntaje promedio
+    const promedio = datos.reduce((suma, recital)=>{
+
+        return suma + Number(recital["puntaje"]);
+
+    },0) / datos.length;
+
+    document.getElementById("puntajePromedio").textContent =
+        promedio.toFixed(1);
+
+    // Recomendados
+    const recomendados = datos.filter(recital =>
+
+        recital["recomendas?"] === "Si"
+
+    ).length;
+
+    document.getElementById("cantRecomendados").textContent =
+        recomendados;
+
+}
+const btnEstadisticas = document.getElementById("btnEstadisticas");
+const btnInicio = document.getElementById("btnInicio");
+
+btnEstadisticas.addEventListener("click",(e)=>{
+
+    e.preventDefault();
+
+    document.getElementById("seccionRecitales").classList.add("d-none");
+
+    document.getElementById("seccionEstadisticas").classList.remove("d-none");
+
+    mostrarEstadisticas(recitales);
+
+});
+
+btnInicio.addEventListener("click",(e)=>{
+
+    e.preventDefault();
+
+    document.getElementById("seccionRecitales").classList.remove("d-none");
+
+    document.getElementById("seccionEstadisticas").classList.add("d-none");
+
+});
